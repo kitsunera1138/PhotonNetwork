ea506a6f038a8fda92b8babec078ee8c7592757a
kitsunera1138 <kitsunera1138@gmail.com>
2024-10-16 19:43:43 +0900

    추가

---
+++ Photon Network Game/Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using UnityEditor.PackageManager.Requests;
+
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+        PhotonNetwork.GameVersion = "1.0f";
+
+        PhotonNetwork.LoadLevel("Lobby Scene");
+    }
+
+    //회원 가입에 성공 했을때 호출되는 콜백함수 
+    public void Success(RegisterPlayFabUserResult registerPlayFabUserResult)
+    {
+        Debug.Log(registerPlayFabUserResult.Request);
+        Debug.Log(registerPlayFabUserResult.ToString());
+    }
+    public void Failure(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+
+    //로그인 함수
+    public void OnSignIn()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(
+            request,
+            Success,
+            Failure
+            );
+    }
+
+    public void OnSighUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(
+            request,
+            Success,
+            Failure
+            );
+    }
+
+
+}
