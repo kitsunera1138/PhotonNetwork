fd59a8aefc1028ac665a279f94350312f2301b5d
kitsunera1138 <kitsunera1138@gmail.com>
2024-10-29 19:43:38 +0900

    추가

--- Photon Network Game/Assets/Scripts/DialogManager.cs
+++ Photon Network Game/Assets/Scripts/DialogManager.cs
@@ -7,6 +7,7 @@ public class DialogManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] InputField inputField;
     [SerializeField] Transform parentTranform;
+    [SerializeField] ScrollRect scrollRect;
 
     void Update()
     {
@@ -19,7 +20,11 @@ public class DialogManager : MonoBehaviourPunCallbacks
 
             //inputField에 있는 텍스트를 가져옵니다.
             //(string 변수) <- Photon 닉네임 + : + inputField에 설정한 문자열
-            string talk = photonView.Owner.NickName + " : " + inputField.text;
+            //문제점 다른사람의 이름이 자신의 이름으로 나타남
+            //string talk = photonView.Owner.NickName + " : " + inputField.text;
+
+            //자신의 고유의 이름을 전달해야 채팅창에 다른사람의 이름이 나타남 
+            string talk = PhotonNetwork.LocalPlayer.NickName + " : " + inputField.text;
 
             //RPC Target.All : 현재 룸에 있는 모든 클라이언트에게 Talk() 함수를 실행하라는 명령을 전달합니다.
             photonView.RPC("Talk", RpcTarget.All, talk);//보내고자하는 함수 이름, 타켓
@@ -40,7 +45,10 @@ public class DialogManager : MonoBehaviourPunCallbacks
         talk.transform.SetParent(parentTranform);
         //채팅을 입력한 후에도 이어서 입력할 수 있도록 설정합니다.
         inputField.ActivateInputField();
+        //스크롤 위치를 초기화합니다.
+        scrollRect.verticalNormalizedPosition = 0.0f;
         //inputField의 텍스트를 초기화 합니다.
         inputField.text = "";
+
     }
 }
